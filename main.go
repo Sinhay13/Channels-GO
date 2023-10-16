38bf43f807086a74ef093a29f44e59b2d9eb94ed
Yahnis allegret <yahnis.allegret@yahoo.com>
2023-10-16 20:51:40 +0900

    first part channel

--- main.go
+++ main.go
@@ -13,19 +13,40 @@ func main() {
 		"http://golang.org",
 		"http://amazon.com",
 	}
-	for _, link := range links {
-		go checkLink(link)
 
+	// create a channel to receive the results
+	c := make(chan string)
+
+	// start a goroutine for each link
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	// receive the results from the channel
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-c)
 	}
 }
 
-func checkLink(link string) {
+// checkLink checks if a given link is up or down by making an HTTP GET request to it.
+// If the request returns an error, it means the link is down, and the function sends a message to the channel.
+// If the request is successful, the link is up, and the function sends a message to the channel.
+func checkLink(link string, c chan string) {
+	// Make an HTTP GET request to the link
 	_, err := http.Get(link)
+
+	// If the request returns an error, the link is down
 	if err != nil {
+		// Print a message to the console
 		fmt.Println(link, "might be down!")
+		// Send a message to the channel
+		c <- link + " might be down!"
 		return
-
 	}
-	fmt.Println(link, "is up!")
 
+	// If the request is successful, the link is up
+	// Print a message to the console
+	fmt.Println(link, "is up!")
+	// Send a message to the channel
+	c <- link + " is up!"
 }
